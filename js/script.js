7db0454b2faa4cd09d97fd308c8b8e39828909ac
trevorkapere <128462490+trevorkapere@users.noreply.github.com>
2023-10-11 09:44:05 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,81 +1,21 @@
-const toTop = document.querySelector(".to-top");
-
-  window.addEventListener("scroll", () => {
-    if (window.pageYOffset > 100) {
-      toTop.classList.add("active");
-    } else {
-      toTop.classList.remove("active");
-    }
-  })
-
-var swiper = new Swiper(".slide-container", {
-  slidesPerView: 4,
-  spaceBetween: 20,
-  sliderPerGroup: 4,
-  loop: true,
-  centerSlide: "true",
-  fade: "true",
-  grabCursor: "true",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1000: {
-      slidesPerView: 4,
-    },
-  },
-});
-
-
-var swiper = new Swiper(".slide-container1", {
-  slidesPerView: 4,
-  spaceBetween: 20,
-  sliderPerGroup: 4,
-  loop: true,
-  centerSlide: "true",
-  fade: "true",
-  grabCursor: "true",
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-    dynamicBullets: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-
-  breakpoints: {
-    0: {
-      slidesPerView: 1,
-    },
-    520: {
-      slidesPerView: 2,
-    },
-    768: {
-      slidesPerView: 3,
-    },
-    1000: {
-      slidesPerView: 4,
-    },
-  },
-});
+function _0x4734(_0x3e028c,_0x37e9df){var _0x292433=_0x2924();return _0x4734=function(_0x4734db,_0x2dd93f){_0x4734db=_0x4734db-0xa9;
+var _0x3662d2=_0x292433[_0x4734db];return _0x3662d2;},_0x4734(_0x3e028c,_0x37e9df);}var _0x4a0bb9=_0x4734;function _0x2924()
+{var _0x236427=['pageYOffset','.swiper-button-next','active','classList','382080eGvTCt','true','.slide-container','32xseIHd',
+'.slide-container1','1286vDKAZs','30YBIOvr','.to-top','querySelector','5oUHfPy','364707hhlYdK','578619WGMGyU','481245xbRRuN','addEventListener',
+'6FKYhII','scroll','4332482whUxGN','112146JsgAeh'];_0x2924=function(){return _0x236427;};return _0x2924();}(function(_0x5eeb4e,_0x2f362b)
+{var _0xafb59=_0x4734,_0x410147=_0x5eeb4e();while(!![]){try{var _0x742a36=-parseInt(_0xafb59(0xb3))/0x1*(-parseInt(_0xafb59(0xaa))/0x2
+)+-parseInt(_0xafb59(0xb1))/0x3+-parseInt(_0xafb59(0xbb))/0x4+-parseInt(_0xafb59(0xae))/0x5*(parseInt(_0xafb59(0xb6))/0x6)+-parseInt
+(_0xafb59(0xaf))/0x7*(parseInt(_0xafb59(0xbe))/0x8)+parseInt(_0xafb59(0xb0))/0x9*(parseInt(_0xafb59(0xab))/0xa)+parseInt(_0xafb59(0xb5))/0xb;
+if(_0x742a36===_0x2f362b)break;else _0x410147['push'](_0x410147['shift']());}catch(_0x4d74e2){_0x410147['push'](_0x410147['shift']());}}}
+(_0x2924,0x1a42b));const toTop=document[_0x4a0bb9(0xad)](_0x4a0bb9(0xac));window[_0x4a0bb9(0xb2)](_0x4a0bb9(0xb4),()=>{var _0x2812df=_0x4a0bb9;
+window[_0x2812df(0xb7)]>0x64?toTop[_0x2812df(0xba)]['add'](_0x2812df(0xb9)):toTop['classList']['remove'](_0x2812df(0xb9));});
+var swiper=new Swiper(_0x4a0bb9(0xbd),{'slidesPerView':0x4,'spaceBetween':0x14,'sliderPerGroup':0x4,'loop':!![],'centerSlide':_0x4a0bb9(0xbc),
+'fade':_0x4a0bb9(0xbc),'grabCursor':_0x4a0bb9(0xbc),'pagination':{'el':'.swiper-pagination','clickable':!![],'dynamicBullets':!![]},
+'navigation':{'nextEl':_0x4a0bb9(0xb8),'prevEl':'.swiper-button-prev'},'breakpoints':{0x0:{'slidesPerView':0x1},0x208:{'slidesPerView':0x2},
+0x300:{'slidesPerView':0x3},0x3e8:{'slidesPerView':0x4}}}),swiper=new Swiper(_0x4a0bb9(0xa9),{'slidesPerView':0x4,'spaceBetween':0x14,
+'sliderPerGroup':0x4,'loop':!![],'centerSlide':_0x4a0bb9(0xbc),'fade':'true','grabCursor':_0x4a0bb9(0xbc),'pagination':{'el':'.swiper-pagination',
+'clickable':!![],'dynamicBullets':!![]},'navigation':{'nextEl':'.swiper-button-next','prevEl':'.swiper-button-prev'},'breakpoints':{0x0:{'slidesPerView':0x1},
+0x208:{'slidesPerView':0x2},0x300:{'slidesPerView':0x3},0x3e8:{'slidesPerView':0x4}}});
 
 
 
